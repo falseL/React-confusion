91c58d57b5b0c52e9ee53e893564aea1a3673fef
Zeyu Li <zeyuli1024@gmail.com>
2019-07-25 20:28:38 -0400

    Intro to Redux

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -77,16 +77,16 @@ function About(props) {
 function RenderLeader({ leader }) {
     return (
         <div key={leader.id} className="col-12 mt-5">
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={leader.image} alt={leader.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{leader.name}</Media>
-                        <p>{leader.designation}</p>
-                        <p>{leader.description}</p>
-                    </Media>
+            <Media tag="li">
+                <Media left middle>
+                    <Media object src={leader.image} alt={leader.name} />
                 </Media>
+                <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                </Media>
+            </Media>
         </div>
     );
 }
